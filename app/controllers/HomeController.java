d211f74dfc29f18e60885404b838be98028ed886
olaganiyu94 <olaganiyu1@macs-MBP.lan>
2016-11-06 00:49:46 +0000

    lab2

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result home(String name) {
+        return ok(home.render("Welcome to the Home Page", name));
+    }
+public Result about() {
+        return ok(about.render());
+    }
+
+public Result product() {
+        return ok(product.render());
+    }
+
+public Result contact() {
+        return ok(contact.render());
+    }
+}
